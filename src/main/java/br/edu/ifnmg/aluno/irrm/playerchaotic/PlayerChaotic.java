938da7783865eed1cdfc3b63957edc9931f7d195
iagor <iagoravide@gmail.com>
2025-08-04 18:58:12 -0300

    Commit inicial - playerchaotic

---
+++ src/main/java/br/edu/ifnmg/aluno/irrm/playerchaotic/PlayerChaotic.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifnmg.aluno.irrm.playerchaotic;
+
+import io.github.guisso.jankenpon.AbstractPlayer;
+import io.github.guisso.jankenpon.Move;
+
+/**
+ *
+ * @author iagor
+ */
+
+// <outputDirectory>C:\\dev\\projects\\javase\\JanKenPonManager\\players</outputDirectory> foi alterado para:
+// <outputDirectory>C:\\Users\\iagor\\Documents\\NetBeansProjects\\JanKenPonManager\\players</outputDirectory>
+public class PlayerChaotic extends AbstractPlayer{
+    
+    @Override
+    public String getDeveloperName(){
+        return "Iago R.R. Maia";
+    }
+
+    @Override
+    public Move makeMyMove(Move opponentPreviousMove) {
+        if(opponentPreviousMove == null){
+            return Move.NONE;
+        }else{
+            return switch (opponentPreviousMove) {
+                case Move.ROCK -> Move.PAPER;
+                case Move.PAPER -> Move.SCISSORS;
+                case Move.SCISSORS -> Move.ROCK;
+                    // default devera ser rock 
+                default -> Move.PAPER;
+            };
+        }
+    }
+    
+}
---
+++ src/main/java/br/edu/ifnmg/aluno/irrm/playerjankenpon/PlayerJanKenPon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifnmg.aluno.irrm.playerjankenpon;
+
+/**
+ *
+ * @author iagor
+ */
+public class PlayerJanKenPon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
